574fbf6f44637baf4d1b01d37513fde76b6f26b1
mccoymd <mccoymatthewd@gmail.com>
2018-07-06 08:50:53 -0400

    running namd on local machine

--- snp2sim.py
+++ snp2sim.py
@@ -5,6 +5,7 @@ import sys
 import argparse
 import copy
 import random
+import multiprocessing
 
 def _parseCommandLine():
 
@@ -38,7 +39,7 @@ def _parseCommandLine():
     parser.add_argument("--simLength",
                         help="varTraj simulation length in ns",
                         action="store",
-                        type=int,
+                        type=float,
                         )
     parser.add_argument("--varResID",
                         help="variant residue ID from PDB template",
@@ -55,6 +56,11 @@ def _parseCommandLine():
                         action="store",
                         type=str,
                         )
+    parser.add_argument("--simProc",
+                        help="number of processors to run simulation",
+                        action="store",
+                        type=int,
+                        )
 
     cmdlineparameters, unknownparams = parser.parse_known_args()
     parameters = copy.copy(cmdlineparameters)
@@ -63,8 +69,8 @@ def _parseCommandLine():
 
 
 def genNAMDstructFiles(parameters):
-    if not os.path.exists("./variantSimulations/%s/bin/" % parameters.protein):
-        os.makedirs("./variantSimulations/%s/bin/" % parameters.protein)
+    if not os.path.exists("%s/variantSimulations/%s/bin/" % (parameters.runDIR,parameters.protein)):
+        os.makedirs("%s/variantSimulations/%s/bin/" % (parameters.runDIR,parameters.protein))
     if os.path.isfile(parameters.templatePDB):
         print "generating solvated/ionized PDB and PSF from %s" \
             % parameters.templatePDB
@@ -77,7 +83,7 @@ def genNAMDstructFiles(parameters):
             os.system(genStructCommand)
 
         genSolvTCL(parameters)
-        genSolvCommand = "%s -e %s" % (parameters.vmdPath, parameters.solvTCL)
+        genSolvCommand = "%s -e %s" % (parameters.VMDpath, parameters.solvTCL)
         os.system(genSolvCommand)
     else:
         print "no template exists"
@@ -132,13 +138,39 @@ def genSolvTCL(parameters):
     return
 
 def genNAMDconfig(parameters):
-    if not os.path.exists("./variantSimulations/%s/config/" % parameters.protein):
-        os.makedirs("./variantSimulations/%s/config/" % parameters.protein)
+    if not os.path.exists("%s/variantSimulations/%s/config/" % (parameters.runDIR,parameters.protein)):
+        os.makedirs("./variantSimulations/%s/config/" % (parameters.runDIR,parameters.protein))
+
+
+    if os.path.isfile(parameters.solvBoundary):
+        boundaryFile = open(parameters.solvBoundary,"r")        
+        bLines = boundaryFile.readlines()
+
+        center = bLines[0]
+        center = center.rstrip()
+        center = center.split(" ")
+        center = [float(i) for i in center]
+        
+        minmax = bLines[1]
+        minmax = minmax.rstrip()
+        minmax = minmax.translate(None,"{}")
+        minmax = minmax.split(" ")
+        minmax = [float(i) for i in minmax]
+
+        parameters.dimX = minmax[3] - minmax[0]
+        parameters.dimY = minmax[4] - minmax[1]
+        parameters.dimZ = minmax[5] - minmax[2]
+
+    else:
+        print "boundary file does not exist"
+        print "remove solvated/ionized PDB and PSF and resubmit"
+        sys.exit()
+        
     configFile = open(parameters.NAMDconfig,"w+")
     configFile.write("structure          %s\n" % parameters.simPSF)
     configFile.write("coordinates        %s\n" % parameters.simPDB)
     configFile.write("set outputname     %s\n" % parameters.NAMDout)
-    configFile.write("paraTypeCharm on\n")
+    configFile.write("paraTypeCharmm on\n")
     for pFile in parameters.simParameters:
         configFile.write("parameters %s\n" % pFile)
     configFile.write("mergeCrossterms yes\n")
@@ -167,28 +199,65 @@ def genNAMDconfig(parameters):
     configFile.write("langevin            on    ;# do langevin dynamics\n")
     configFile.write("langevinDamping     1     ;# damping coefficient (gamma) of 1/ps\n")
     configFile.write("langevinTemp        $temperature\n")
-    configFile.write("langevinHydrogen    off    ;# don't couple langevin bath to hydrogens\n")
-    
-    
+    configFile.write("langevinHydrogen    off    ;# don't couple langevin bath to hydrogens\n\n")
+    configFile.write("#Periodic Boundary Conditions\n")
+    configFile.write("cellBasisVector1 %.2f 0.0 0.0\n" % parameters.dimX)
+    configFile.write("cellBasisVector2 0.0 %.2f 0.0\n" % parameters.dimY)
+    configFile.write("cellBasisVector3 0.0 0.0 %.2f\n" % parameters.dimZ)
+    configFile.write("cellOrigin %.2f %.2f %.2f\n\n" % \
+                     (center[0], center[1], center[0]))
+    configFile.write("wrapAll on\n\n")
+    configFile.write("# PME (for full-system periodic electrostatics)\n")
+    configFile.write("PME                 yes\n")
+    configFile.write("PMEGridSpacing      1.0\n")
+    configFile.write("# Constant Pressure Control (variable volume)\n")
+    configFile.write("useGroupPressure      yes ;# needed for rigidBonds\n")
+    configFile.write("useFlexibleCell       no\n")
+    configFile.write("useConstantArea       no\n")
+    configFile.write("langevinPiston        on\n")
+    configFile.write("langevinPistonTarget  1.01325 ;#  in bar -> 1 atm\n")
+    configFile.write("langevinPistonPeriod  100.0\n")
+    configFile.write("langevinPistonDecay   50.0\n")
+    configFile.write("langevinPistonTemp    $temperature\n")
+    configFile.write("# Output\n")
+    configFile.write("outputName          $outputname\n")
+    #todo - configure output options i.e. freq of output)
+    configFile.write("restartfreq         10000     ;# 500steps = every 1ps\n")
+    configFile.write("dcdfreq             5000\n")
+    configFile.write("xstFreq             5000\n")
+    configFile.write("outputEnergies      5000\n")
+    configFile.write("outputPressure      5000\n")
+    configFile.write("# Minimization\n")
+    configFile.write("minimize            1000\n")
+    configFile.write("reinitvels          $temperature\n\n")
+    configFile.write("run                 %i\n" % (parameters.simLength*50000)) # using 2 fs step size
+                     
+
     
     return
+
+
+#### start of program move to main()
 
 parameters = _parseCommandLine()
 
 ### Hardcoded Parameters
 
+parameters.runDIR = os.getcwd()
+print(parameters.runDIR)
+
 if parameters.protein:
     print parameters.protein
-    parameters.vmdPath = "vmd"
-    parameters.namdPath = "namd"
+    parameters.VNDpath = "vmd"
+    parameters.NAMDpath = "namd2"
     if parameters.mode == "varMDsim":
         if not parameters.simID:
-            parameters.simID = random.randint(1,1000)
+            parameters.simID = str(random.randint(1,1000))
             
-        parameters.simTopology = ("./simParameters/top_all36_prot.rtf",
-                                  "./simParameters/toppar_water_ions_namd.str")
-        parameters.simParameters = ("./simParameters/par_all36_prot.prm",
-                                    "./simParameters/toppar_water_ions_namd.str")
+        parameters.simTopology = ("%s/simParameters/top_all36_prot.rtf" % parameters.runDIR,
+                                  "%s/simParameters/toppar_water_ions_namd.str" % parameters.runDIR)
+        parameters.simParameters = ("%s/simParameters/par_all36_prot.prm" % parameters.runDIR,
+                                    "%s/simParameters/toppar_water_ions_namd.str" %parameters.runDIR)
         if parameters.varResID and parameters.varAA:
             parameters.variant = parameters.varResID + parameters.varAA
         else:
@@ -196,37 +265,37 @@ if parameters.protein:
             print"Using WT structure for simulation"
             parameters.variant = "wt"
             
-        parameters.simPDB = "./variantSimulations/%s/structures/%s.%s.pdb" \
-                            % (parameters.protein,parameters.protein,parameters.variant)
-        parameters.simPSF = "./variantSimulations/%s/structures/%s.%s.psf" \
-                            % (parameters.protein,parameters.protein,parameters.variant)
-        parameters.templatePDB = "./variantSimulations/%s/structures/%s.template.pdb" \
-                                 % (parameters.protein,parameters.protein)
-        parameters.wtPDB = "./variantSimulations/%s/structures/%s.wt.UNSOLVATED.pdb" \
-                             % (parameters.protein, parameters.protein)
-        parameters.wtPSF = "./variantSimulations/%s/structures/%s.wt.UNSOLVATED.psf" \
-                             % (parameters.protein, parameters.protein)
-        parameters.varPrefix = "./variantSimulations/%s/structures/%s.%s.UNSOLVATED" \
-                               % (parameters.protein, parameters.protein, parameters.variant)
-        parameters.varPDB = "./variantSimulations/%s/structures/%s.%s.UNSOLVATED.pdb" \
-                             % (parameters.protein, parameters.protein, parameters.variant)
-        parameters.varPSF = "./variantSimulations/%s/structures/%s.%s.UNSOLVATED.psf" \
-                             % (parameters.protein, parameters.protein, parameters.variant)
-        parameters.wtStructTCL = "./variantSimulations/%s/bin/%s.wt.genStructFiles.tcl" \
-                                 % (parameters.protein, parameters.protein)
-        parameters.varStructTCL = "./variantSimulations/%s/bin/%s.%s.genStructFiles.tcl" \
-                                  % (parameters.protein, parameters.protein, parameters.variant)
-        parameters.solvTCL = "./variantSimulations/%s/bin/%s.%s.genSolvStruct.tcl" \
-                             % (parameters.protein, parameters.protein, parameters.variant)
-        parameters.solvBoundary = "./variantSimulations/%s/bin/%s.%s.solvBoundary.txt" \
-                                  % (parameters.protein, parameters.protein, parameters.variant)
-        parameters.structPrefix = "./variantSimulations/%s/structures/%s.%s" \
-                                  % (parameters.protein, parameters.protein, parameters.variant)
-        parameters.NAMDconfig = "./variantSimulations/%s/config/%s.%s.%s.NAMD" \
-                                  % (parameters.protein, parameters.protein,
+        parameters.simPDB = "%s/variantSimulations/%s/structures/%s.%s.pdb" \
+                            % (parameters.runDIR,parameters.protein,parameters.protein,parameters.variant)
+        parameters.simPSF = "%s/variantSimulations/%s/structures/%s.%s.psf" \
+                            % (parameters.runDIR,parameters.protein,parameters.protein,parameters.variant)
+        parameters.templatePDB = "%s/variantSimulations/%s/structures/%s.template.pdb" \
+                                 % (parameters.runDIR,parameters.protein,parameters.protein)
+        parameters.wtPDB = "%s/variantSimulations/%s/structures/%s.wt.UNSOLVATED.pdb" \
+                             % (parameters.runDIR,parameters.protein, parameters.protein)
+        parameters.wtPSF = "%s/variantSimulations/%s/structures/%s.wt.UNSOLVATED.psf" \
+                             % (parameters.runDIR,parameters.protein, parameters.protein)
+        parameters.varPrefix = "%s/variantSimulations/%s/structures/%s.%s.UNSOLVATED" \
+                               % (parameters.runDIR,parameters.protein, parameters.protein, parameters.variant)
+        parameters.varPDB = "%s/variantSimulations/%s/structures/%s.%s.UNSOLVATED.pdb" \
+                             % (parameters.runDIR, parameters.protein, parameters.protein, parameters.variant)
+        parameters.varPSF = "%s/variantSimulations/%s/structures/%s.%s.UNSOLVATED.psf" \
+                             % (parameters.runDIR,parameters.protein, parameters.protein, parameters.variant)
+        parameters.wtStructTCL = "%s/variantSimulations/%s/bin/%s.wt.genStructFiles.tcl" \
+                                 % (parameters.runDIR,parameters.protein, parameters.protein)
+        parameters.varStructTCL = "%s/variantSimulations/%s/bin/%s.%s.genStructFiles.tcl" \
+                                  % (parameters.runDIR,parameters.protein, parameters.protein, parameters.variant)
+        parameters.solvTCL = "%s/variantSimulations/%s/bin/%s.%s.genSolvStruct.tcl" \
+                             % (parameters.runDIR,parameters.protein, parameters.protein, parameters.variant)
+        parameters.solvBoundary = "%s/variantSimulations/%s/bin/%s.%s.solvBoundary.txt" \
+                                  % (parameters.runDIR,parameters.protein, parameters.protein, parameters.variant)
+        parameters.structPrefix = "%s/variantSimulations/%s/structures/%s.%s" \
+                                  % (parameters.runDIR,parameters.protein, parameters.protein, parameters.variant)
+        parameters.NAMDconfig = "%s/variantSimulations/%s/config/%s.%s.%s.NAMD" \
+                                  % (parameters.runDIR, parameters.protein, parameters.protein,
                                      parameters.variant, parameters.simID)
-        parameters.NAMDout = "./variantSimulations/%s/results/%s/trajectory/%s.%s.%s" \
-                             % (parameters.protein, parameters.variant,
+        parameters.NAMDout = "%s/variantSimulations/%s/results/%s/trajectory/%s.%s.%s" \
+                             % (parameters.runDIR, parameters.protein, parameters.variant,
                                 parameters.protein, parameters.variant, parameters.simID)
                                 
         
@@ -239,12 +308,12 @@ else:
 if parameters.mode == "varMDsim":
     print "Performing varMDsim"
     if parameters.newStruct:
-        if os.path.isdir("./variantSimulations/%s" % parameters.protein):
+        if os.path.isdir("%s/variantSimulations/%s" % (parameters.runDIR,parameters.protein)):
             print "ERROR - %s is already created... resubmit with new protien name"
             sys.exit()
-        os.makedirs("./variantSimulations/%s/structures" % parameters.protein)
-        os.system("cp %s ./variantSimulations/%s/structures/%s.template.pdb" \
-                  % (parameters.newStruct, parameters.protein, parameters.protein)) 
+        os.makedirs("%s/variantSimulations/%s/structures" % (parameters.runDIR,parameters.protein))
+        os.system("cp %s %s/variantSimulations/%s/structures/%s.template.pdb" \
+                  % (parameters.runDIR,parameters.newStruct, parameters.protein, parameters.protein)) 
 
     if os.path.isfile(parameters.simPDB):
         if os.path.isfile(parameters.simPSF):
@@ -262,9 +331,24 @@ if parameters.mode == "varMDsim":
         genNAMDstructFiles(parameters)
 
     if parameters.simLength:
-        print "Performing Variant %ins Simulation" % parameters.simLength
+        print "Performing Variant %.3f ns Simulation" % parameters.simLength
+        parameters.simLength = parameters.simLength
         genNAMDconfig(parameters)
-        #todo run NAMD simulation
+
+        if not parameters.simProc:
+            parameters.simProc = multiprocessing.cpu_count()    
+        runNAMDcommand = "%s +p%i %s > %s.log" % \
+                         (parameters.NAMDpath, parameters.simProc,
+                          parameters.NAMDconfig, parameters.NAMDout)
+        if not os.path.isdir("%s/variantSimulations/%s/results/%s/trajectory/" \
+                             % (parameters.runDIR, parameters.protein, parameters.variant)):
+            os.makedirs("%s/variantSimulations/%s/results/%s/trajectory/" \
+                             % (parameters.runDIR, parameters.protein, parameters.variant))
+
+
+        print "running NAMD with %i processors" % parameters.simProc
+        os.system(runNAMDcommand)
+        
     else:
         print "simulation length not specified"        
         sys.exit()
