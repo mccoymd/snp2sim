27b6241a332a8e2e94bffbb465ed4e35e2e5e53a
mccoymd <mccoymatthewd@gmail.com>
2019-08-23 13:41:35 -0400

    updated implicit solvent config

--- snp2sim.py
+++ snp2sim.py
@@ -332,6 +332,12 @@ def _parseCommandLine():
 						help="use implicit solvent instead of water",
 						action="store_true",
 						)
+	parser.add_argument("--margin",
+						help="value of margin to use in NAMD simulation ",
+						action="store",
+						default="1.0",
+						type=str,
+						)
 
 	#varScaff options
 	parser.add_argument("--scaffID",
@@ -605,6 +611,13 @@ def runNAMD(parameters):
 	configFile.write("temperature         $temperature\n")
 	configFile.write("\n")
 	configFile.write("\n")
+	if parameters.implicitSolvent:
+		configFile.write("# Implicit Solvant Parameters\n")
+		configFile.write("gbis                scaled1-4\n")
+		configFile.write("alphaCutoff         12.0\n")
+		configFile.write("ionConcentration    0.3\n")
+		configFile.write("\n")
+		configFile.write("\n")
 	configFile.write("# Force-Field Parameters\n")
 	configFile.write("exclude             scaled1-4\n")
 	configFile.write("1-4scaling          1.0\n")
@@ -629,30 +642,28 @@ def runNAMD(parameters):
 	configFile.write("langevinDamping     1     ;# damping coefficient (gamma) of 1/ps\n")
 	configFile.write("langevinTemp        $temperature\n")
 	configFile.write("langevinHydrogen    off    ;# don't couple langevin bath to hydrogens\n\n")
-    
-	configFile.write("#Periodic Boundary Conditions\n")
-	configFile.write("cellBasisVector1 %.1f 0.0 0.0\n" % parameters.dimX)
-	configFile.write("cellBasisVector2 0.0 %.1f 0.0\n" % parameters.dimY)
-	configFile.write("cellBasisVector3 0.0 0.0 %.1f\n" % parameters.dimZ)
-	configFile.write("cellOrigin %.1f %.1f %.1f\n" % \
-						(center[0], center[1], center[2]))
-	configFile.write("margin 1.0\n\n")
-	if parameters.implicitSolvent:
-		configFile.write("GBIS       on\n")
-	else:
+    if not parameters.implicitSolvent:
+		configFile.write("#Periodic Boundary Conditions\n")
+		configFile.write("cellBasisVector1 %.1f 0.0 0.0\n" % parameters.dimX)
+		configFile.write("cellBasisVector2 0.0 %.1f 0.0\n" % parameters.dimY)
+		configFile.write("cellBasisVector3 0.0 0.0 %.1f\n" % parameters.dimZ)
+		configFile.write("cellOrigin %.1f %.1f %.1f\n" % \
+							(center[0], center[1], center[2]))
+	configFile.write("margin                %s\n\n" % parameters.margin)
+	if not parameters.implicitSolvent:
 		configFile.write("# PME (for full-system periodic electrostatics)\n")
 		configFile.write("PME                 yes\n")
 		configFile.write("PMEGridSpacing      1.0\n")
-	configFile.write("wrapAll on\n\n")
-	configFile.write("# Constant Pressure Control (variable volume)\n")
-	configFile.write("useGroupPressure      yes ;# needed for rigidBonds\n")
-	configFile.write("useFlexibleCell       no\n")
-	configFile.write("useConstantArea       no\n")
-	configFile.write("langevinPiston        on\n")
-	configFile.write("langevinPistonTarget  1.01325 ;#  in bar -> 1 atm\n")
-	configFile.write("langevinPistonPeriod  100.0\n")
-	configFile.write("langevinPistonDecay   50.0\n")
-	configFile.write("langevinPistonTemp    $temperature\n")
+		configFile.write("wrapAll on\n\n")
+		configFile.write("# Constant Pressure Control (variable volume)\n")
+		configFile.write("useGroupPressure      yes ;# needed for rigidBonds\n")
+		configFile.write("useFlexibleCell       no\n")
+		configFile.write("useConstantArea       no\n")
+		configFile.write("langevinPiston        on\n")
+		configFile.write("langevinPistonTarget  1.01325 ;#  in bar -> 1 atm\n")
+		configFile.write("langevinPistonPeriod  100.0\n")
+		configFile.write("langevinPistonDecay   50.0\n")
+		configFile.write("langevinPistonTemp    $temperature\n")
 	configFile.write("# Output\n")
 	configFile.write("outputName          $outputname\n")
 	#todo - configure output options i.e. freq of output)
